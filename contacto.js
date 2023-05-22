28da7dd9900c8cf519c464b2a27b9da9f159932e
NicolasVillanueva1 <nicolasvillanueva98@gmail.com>
2023-05-21 23:08:55 -0300

    Puta función

--- contacto.js
+++ contacto.js
@@ -1,6 +1,6 @@
 const expresiones = {
         usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-        nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/; // Letras y espacios, pueden llevar acentos.
+        nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
         password: /^.{8,40}$/, // 8 a 40 digitos.
         correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
         telefono: /^\d{7,14}$/ // 7 a 14 numeros.
@@ -22,8 +22,20 @@ function validarFormulario() {
         alert("Todos los campos son obligatorios");
         return false;
     }
-    else if (validarNombre(nombre) === false) {
+    else if ((fname.test(nombre) === false) || (fname.lenght > 40)) {
         alert("El Nombre ingresado no es valido");
+        return false;
+    }
+    else if ((lname.test(apellido) === false) || (fname.lenght > 40)) {
+        alert("El Apellido ingresado no es valido");
+        return false;
+    }
+    else if (email.test(correo) === false) {
+        alert("El Correo ingresado no es valido");
+        return false;
+    }
+    else if ((isNaN(phone) === false) || (phone.lenght > 14) || (phone < 7)) {
+        alert("El Teléfono ingresado no es valido");
         return false;
     }
 }
