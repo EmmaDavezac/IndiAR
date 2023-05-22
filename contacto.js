b0b7880ad30d451b3d49eeed2288098e31e66155
NicolasVillanueva1 <nicolasvillanueva98@gmail.com>
2023-05-22 16:39:33 -0300

    a

--- contacto.js
+++ contacto.js
@@ -1,42 +1,58 @@
 const expresiones = {
-        usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-        nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-        password: /^.{8,40}$/, // 8 a 40 digitos.
-        correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-        telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+    password: /^.{8,40}$/, // 8 a 40 digitos.     
 }
+
 function validarNombre(input){
-    if (input.test(nombre)===false || input.lenght > 40) {
+    let nombre = /^[a-zA-ZÀ-ÿ\s]{1,40}$/; // Letras y espacios, pueden llevar acentos.
+    if (nombre.test(input)===false || input.lenght > 40) {
+        return false;
+    }
+}
+function validarApellido(input){
+    let apellido = /^[a-zA-ZÀ-ÿ\s]{1,40}$/; // Letras y espacios, pueden llevar acentos.
+    if (apellido.test(input)===false || input.lenght > 40) {
+        return false;
+    }
+}
+function validarEmail(input){
+    let correo = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/; // Letras, números, símbolos especiales
+    if (correo.test(input)===false || input.lenght > 40) {
+        return false;
+    }
+}
+
+function validarTelefono(input){
+    if (isNaN(input)===false || input.lenght < 7 || input.lenght > 40) {
         return false;
     }
 }
 
 function validarFormulario() {
-    var fname, lname, email, phone, message;
+    let aceptado = false;
+    let alertas = "";
+    let fname, lname, email, phone, message;
     fname = document.getElementById("fname").value;
     lname = document.getElementById("lname").value;
     email = document.getElementById("email").value;
     phone = document.getElementById("phone").value;
     message = document.getElementById("message").value;
-    if (nombre === "" || apellido === "" || correo === "" || telefono === "" || mensaje === "") {
-        alert("Todos los campos son obligatorios");
-        return false;
-    }
-    else if ((fname.test(nombre) === false) || (fname.lenght > 40)) {
-        alert("El Nombre ingresado no es valido");
-        return false;
-    }
-    else if ((lname.test(apellido) === false) || (fname.lenght > 40)) {
-        alert("El Apellido ingresado no es valido");
-        return false;
-    }
-    else if (email.test(correo) === false) {
-        alert("El Correo ingresado no es valido");
-        return false;
-    }
-    else if ((isNaN(phone) === false) || (phone.lenght > 14) || (phone < 7)) {
-        alert("El Teléfono ingresado no es valido");
-        return false;
-    }
-}
+    warnings = document.getElementById("wanings");
 
+    if (fname === "" || lname === "" || email === "" || phone === "" || message === "") {
+        alertas += `Todos los campos son obligatorios`;
+        aceptado = true;
+    }
+    else if (validarNombre(fname) === false) {
+        alertas += "El 'Nombre' ingresado no es valido"; 
+        aceptado = true;
+    }
+    else if (validarApellido(lname === false)) {
+        alertas += "El 'Apellido' ingresado no es valido";
+        aceptado = true;
+    }
+    if (aceptado) { 
+        warnings. = warnings;
+    }
+
+}
