5d0446bcd89b4848ba5021f83383aebe39f0aeb1
Emmanuel Davezac <lucianodavezac@gmail.com>
2023-05-24 13:18:49 -0300

    consumicion de la api rawg

--- script-game-page.js
+++ script-game-page.js
@@ -1,12 +1,10 @@
+//Interacciones para cambiar de imagen
 let gameThumb1=document.getElementById("game-thumb-1");
 let gameThumb2=document.getElementById("game-thumb-2");
 let gameThumb3=document.getElementById("game-thumb-3");
 let gameThumb4=document.getElementById("game-thumb-4");
 let gameThumb5=document.getElementById("game-thumb-5");
-let videoChange=document.getElementById("main-game-video");
-let videoSrc=videoChange.src;
 let imgChange=document.getElementById("main-game-img");
-let activeThumb=1;
 let colorBorde="#ff4e00"
 
 function eliminarBordes()
@@ -17,50 +15,87 @@ let elementos=document.getElementsByClassName("game-thumb");
     }
 }
 
-
-
 gameThumb1.onclick=function(){
 eliminarBordes();
 gameThumb1.style.borderColor=colorBorde;
-videoChange.src=videoSrc;
-imgChange.style.display="none";
-videoChange.style.display="block"
-activeThumb=1;
+imgChange.src=gameThumb1.src;
 }
+
 gameThumb2.onclick=function(){
 eliminarBordes();
 gameThumb2.style.borderColor=colorBorde;
-
 imgChange.src=gameThumb2.src;
-imgChange.style.display="block";
-videoChange.style.display="none"
-
-    video=document.getElementById('main-game-video');
-    video.pause();
-
 }
+
 gameThumb3.onclick=function(){
 eliminarBordes();
 gameThumb3.style.borderColor=colorBorde;
 imgChange.src=gameThumb3.src;
-imgChange.style.display="block";
-videoChange.style.display="none"
 }
+
 gameThumb4.onclick=function(){
 eliminarBordes();
 gameThumb4.style.borderColor=colorBorde;
-
 imgChange.src=gameThumb4.src;
-imgChange.style.display="block";
-videoChange.style.display="none"
 }
+
 gameThumb5.onclick=function(){
 eliminarBordes();
 gameThumb5.style.borderColor=colorBorde;
 imgChange.src=gameThumb5.src;
-imgChange.style.display="block";
-videoChange.style.display="none"
 }
-
+//eliminamos el de resaltado de todas las imagenes y resaltamos la primera imagen
 eliminarBordes();
 gameThumb1.style.borderColor=colorBorde;
+
+let apiKey="fb0a1f24f60743148908dba199441e4d";
+let TituloDelJuego=window.location.pathname.split('/').pop().split('.')[0];
+
+// Realizar una solicitud a la API para obtener información de un juego
+fetch("https://api.rawg.io/api/games/"+TituloDelJuego+"?key="+apiKey)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    let gamePublisher=document.getElementById("game-publisher");
+    gamePublisher.textContent= data.publishers[0].name;
+    let gameDeveloper=document.getElementById("game-developer");
+    gameDeveloper.textContent= data.developers[0].name;
+    let releaseDate=document.getElementById("release-date");
+    releaseDate.textContent=data.released;
+
+    let gameTitle=document.getElementById("game-title");
+    gameTitle.textContent=data.name;
+    let gameDescription=document.getElementById("game-description");
+    gameDescription.textContent=data.description_raw;
+    let buyCardThumb=document.getElementById("buy-card-thumb");
+    buyCardThumb.src=data.background_image;
+  })
+  .catch(error => {
+    // Manejar errores de la solicitud
+    console.log('Error:', error);
+  });
+// Realizar una solicitud a la API para obtener screenshots del juego
+fetch("https://api.rawg.io/api/games/"+TituloDelJuego+"/screenshots?key="+apiKey)
+  .then(response => response.json())
+  .then(data => {
+    // Manejar los datos de respuesta
+    console.log("imagenes");
+    console.log(data);
+    gameThumb1.src=data.results[0].image;
+    gameThumb2.src=data.results[1].image;
+    gameThumb3.src=data.results[2].image;
+    gameThumb4.src=data.results[3].image;
+    gameThumb5.src=data.results[4].image;
+    imgChange.src=gameThumb1.src;
+  })
+  .catch(error => {
+    // Manejar errores de la solicitud
+    console.log('Error:', error);
+  });
+
+  
+  
+
+
+
+
